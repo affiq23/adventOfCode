4e95cdcb53fa1ee0decca4f5f3ff64a9a3da40be
affiq23 <affiqem@gmail.com>
2022-12-06 21:22:37 -0600

    initial commit

---
+++ day1/day1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+int main(){
+
+ int largestElf = 0;
+
+    fstream textFile;
+    textFile.open("input.txt", ios::in);
+
+    if (textFile.is_open()) {
+        int val = 0, max = 0;
+       
+        string line;
+        while(getline(textFile, line)){
+            if(line.empty()){
+                val = 0;
+            } else {
+                val = val + stoi(line);
+                if(val > max){
+                    max = val;
+                }                   
+            }
+        }
+        textFile.close();
+        cout << "Largest elf: " << max << endl;
+    }
+    
+    return 0;
+}
+
